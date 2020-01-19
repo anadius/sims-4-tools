8a3e416c277944aca3bc45ae809ac514889947be
anadius <anadius@gmail.com>
2020-01-19 12:25:04 +0100

    add hello fix

---
+++ docs/hello-error-fix.bat
@@ -0,0 +1,2 @@
+powershell -command "Set-Processmitigation -Name TS4_x64.exe -Disable CFG, ForceRelocateImages"
+powershell -command "Set-Processmitigation -Name TS4.exe -Disable CFG, ForceRelocateImages"
